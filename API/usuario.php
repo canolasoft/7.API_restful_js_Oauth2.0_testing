8a43101a6a7eb118a507090c6e2c3b12577b6468
Fernando Walpert <walllpapert@gmail.com>
2025-08-16 10:12:07 -0300

    cambios
    pequeños ajustes

--- API/usuario.php
+++ API/usuario.php
@@ -98,7 +98,6 @@ class Usuario
 			if(mysqli_num_rows($result) > 0){
 				$usuario = mysqli_fetch_assoc($result);
 				if(password_verify($usr_pass, $usuario['usr_pass'])){
-					//return $usuario; // Retorna el usuario si las credenciales son correctas
 					$usr_key = md5($usuario['usr_email'] . $usuario['usr_pass'] . time()); // Genera una clave de usuario
 					// registro la key en la base de datos
 					// verifico si ya existe un token para este usuario
